8d26a2138eacdfac1ec86f6d5cef9531d431e96e
sheikmohamedali98 <103345551+sheikmohamedali98@users.noreply.github.com>
2022-07-19 22:43:23 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -23,9 +23,12 @@ public class Patterns {
 
 	/*
 	 * Right Angle Triangle Pattren
-	 * 
-	 * * * * * * * * * * *
-	 * 
+	 
+	 *  
+	 * * 
+	 * * *
+	 * * * * 
+	 * * * * *
 	 * 
 	 */
 
@@ -381,6 +384,31 @@ public class Patterns {
 			}
 			for (int col = 1; col <= print; col++) {
 				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	/*
+	 * 
+	 *    
+	1
+   121
+  12321
+ 1234321
+123454321
+
+	 */
+	public void numberPrymidPatten(int n){
+		
+		for(int row=1;row<=n;row++){
+			for(int space=1;space<=n-row;space++){
+				System.out.print(" ");
+			}
+			for(int col=1;col<=row;col++){
+				System.out.print(col);
+			}
+			for(int i=row-1;i>=1;i--){
+				System.out.print(i);
 			}
 			System.out.println();
 		}
